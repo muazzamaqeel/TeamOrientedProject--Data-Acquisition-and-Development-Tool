02e89f1ac5fcc0a435143e13f48d0f1473129f28
Dudikos <duygulenovoL16@outlook.com>
2024-10-29 10:22:59 +0100

    Create HeaderCheck.cs

---
+++ Testing_Integration_Environment/SmartPacifier - TestingFramework/SmartPacifier - TestingFramework/UITests/UI_Tests-FrontEnd/UI_Tests_Tabs/UI_Test_Sidebar/HeaderCheck.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartPacifier___TestingFramework.UITests.UI_Tests_FrontEnd.UI_Tests_Tabs.UI_Test_Sidebar
+{
+    internal class HeaderCheck
+    {
+    }
+}
